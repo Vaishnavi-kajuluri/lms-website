9cc0c3a2a84797eb1c2be7d21ce58e7b823b7327
Vaishnavi-kajuluri <vaishukajuluri69@gmail.com>
2025-07-10 01:47:12 +0530

    completed frontend

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -10,7 +10,10 @@ const Navbar = () => {
    const {user}=useUser();
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${iscourselistpage ?'bg-white':'bg-cyan-100/70'}`}>
-       <img onClick={()=>navigate('/')} src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
+     <Link to="/">
+  <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
+</Link>
+
        {/*for desktop screens */}
        <div className='hidden md:flex items-center gap-5 text-gray-500'>
         <div className='flex gap-5 items-center'>
